c3dba14b27f790b7df1cb616c4b5c25bf45990f3
yeonghoon.lee <djaakclsrn12@naver.com>
2024-06-15 19:32:54 +0900

    fix: 공휴일 조회 API 오류로 주석처리

--- index.js
+++ index.js
@@ -10,14 +10,18 @@ async function checkSitesFeeds() {
   const dateNow = new Date();
   console.group(dateNow.toLocaleString());
 
-  if(await checkHoliday(dateNow)) return;
+  // TODO: 간할적 "SERVICE_KEY_IS_NOT_REGISTERED_ERROR" 에러로 임시 주석처리
+  // 오픈API 서버 불안정 추정
+  // if(await checkHoliday(dateNow)) return;
 
   console.log(":: Start Check Oneul Menu");
   const oneulMenu = await checkOneul();
+  // const oneulMenu = "https://scontent-ssn1-1.cdninstagram.com/v/t51.29350-15/448166969_1424889524893717_1380347787249398248_n.heic?stp=c0.141.1131.1131a_dst-jpg_e35_s640x640_sh0.08&_nc_ht=scontent-ssn1-1.cdninstagram.com&_nc_cat=101&_nc_ohc=f60pAflcJa0Q7kNvgFDEPUZ&edm=AOQ1c0wBAAAA&ccb=7-5&oh=00_AYC1cBjdxtGO-i80pbvWGhfHCZ77X6XUMHOavPTAci9f2A&oe=666EDA8F&_nc_sid=8b3546";
   console.log(":: End Check Oneul Menu (" + oneulMenu.length + ")");
 
   console.log(":: Start Check Jungban Menu");
   const jungbanMenu = await checkJungban();
+  // const jungbanMenu = "https://scontent-ssn1-1.cdninstagram.com/v/t51.29350-15/448166969_1424889524893717_1380347787249398248_n.heic?stp=c0.141.1131.1131a_dst-jpg_e35_s640x640_sh0.08&_nc_ht=scontent-ssn1-1.cdninstagram.com&_nc_cat=101&_nc_ohc=f60pAflcJa0Q7kNvgFDEPUZ&edm=AOQ1c0wBAAAA&ccb=7-5&oh=00_AYC1cBjdxtGO-i80pbvWGhfHCZ77X6XUMHOavPTAci9f2A&oe=666EDA8F&_nc_sid=8b3546";
   console.log(":: End Check Jungban Menu(" + jungbanMenu.length + ")");
 
   if (oneulMenu.length > 0) {
