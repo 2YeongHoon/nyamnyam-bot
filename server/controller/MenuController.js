630fea768651426966d0f5fbd9d8b732fd6f3d5c
yeonghoon.lee <djaakclsrn12@naver.com>
2024-06-21 23:14:37 +0900

    feat: 공휴일 정보 추가

--- server/controller/MenuController.js
+++ server/controller/MenuController.js
@@ -1,9 +1,17 @@
 import express from 'express';
 import { checkSitesFeeds } from '../service/SlackService.js';
+import { checkHoliday } from '../utils/holiday.js';
 
 const router = express.Router()
 
 router.get('/', (req, res) => {
+  const dateNow = new Date();
+  console.group(dateNow.toLocaleString());
+
+  if(checkHoliday(dateNow)) {
+    res.send("fail");
+    return;
+  }
   checkSitesFeeds();
   res.send('ok');
 });
